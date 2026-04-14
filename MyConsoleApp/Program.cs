7cf32215ff6ce1ef8faea26bd7f0fd59776d15aa
Ramesh <avinashpasupureddi494@gmail.com>
2026-04-14 17:52:24 +0530

    Added public and hi all

--- MyConsoleApp/Program.cs
+++ MyConsoleApp/Program.cs
@@ -1,10 +1,11 @@
 ﻿namespace MyConsoleApp
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hi all");
         }
     }
 }
