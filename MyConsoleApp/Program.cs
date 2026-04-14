f99b2d3443a8fc75bb2e0495cec7d45b06114e51
Ramesh <avinashpasupureddi494@gmail.com>
2026-04-14 16:38:48 +0530

    added console statement

--- MyConsoleApp/Program.cs
+++ MyConsoleApp/Program.cs
@@ -1,9 +1,10 @@
 ﻿namespace MyConsoleApp
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello, World!");
             Console.WriteLine("Hello, World!");
         }
     }
